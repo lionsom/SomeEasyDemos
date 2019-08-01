b1ea545258426adf5c4442ff44f7caca75b2eac9
启业云 <qiyeyun@qiyeyundeMac-mini.local>
2019-08-01 17:34:50 +0800

    多继承

---
+++ Objective-C多继承/Objective-C多继承/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Objective-C多继承
+//
+//  Created by 启业云 on 2019/8/1.
+//  Copyright © 2019 启业云. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
