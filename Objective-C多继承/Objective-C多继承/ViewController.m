0dd5223138256d30a9036b5b541a0c6ba523ebb1
启业云 <qiyeyun@qiyeyundeMac-mini.local>
2019-08-02 19:45:45 +0800

    AA

--- Objective-C多继承/Objective-C多继承/ViewController.m
+++ Objective-C多继承/Objective-C多继承/ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 
+#import "Programmer.h"
+#import "Programmer+Coding.h"
+
+#import "Student.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +22,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+/**
+ 多继承实现一：分类，给类添加属性和方法
+ */
+    Programmer *programmer = [[Programmer alloc] init];
+    programmer.height = @"123";
+    [programmer show];
+    
+/**
+ 多继承实现二：协议
+    1.子类需要实现协议方法
+    2.由于协议无法定义属性，所以该方法只能实现方法的多继承
+ */
+    Student *stu = [Student new];
+    [stu running];
+    [stu chatting];
+    
 }
 
 
